5c632e8cf3dd752124e01ea923257e3b2d3bef1f
Patryk Dumin <patrykom01@gmail.com>
2023-11-10 23:20:48 +0100

    add Model and controller aka repository

---
+++ src/main/java/io/github/zypper007/todoapp/model/Task.java
@@ -0,0 +1,40 @@
+package io.github.zypper007.todoapp.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String description;
+    private boolean done;
+
+    Task() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    void setDone(boolean done) {
+        this.done = done;
+    }
+}
